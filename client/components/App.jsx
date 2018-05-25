f37995e3360f127f5ef7950425d9f6bdbf6d78fd
meakommo <meako98@gmail.com>
2018-05-26 09:50:14 +1200

    created dev branch don't push to master

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import NavBar from './NavBar'
-import AllPosts from './AllPosts'
 
 class App extends React.Component {
   render () {
