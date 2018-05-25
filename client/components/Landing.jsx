d15645dcb6013e9021122d31a5cf5299d636d5e7
Frank <xuzy213@gmail.com>
2018-05-26 11:59:52 +1200

    add placeholder into Landing component

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -4,7 +4,60 @@ class Landing extends Component {
   render () {
     return (
       <div className="column is-main-content">
-      Main content
+        <div className="tile is-ancestor">
+          <div className="tile is-vertical is-11">
+            <div className="tile is-parent">
+              <article className="tile is-child notification is-danger">
+                <p className="title">Wide tile</p>
+                <p className="subtitle">Aligned with the right tile</p>
+                <div className="content">
+                </div>
+              </article>
+            </div>
+            <div className="tile">
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+            </div>
+            <div className="tile">
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+            </div>
+            <div className="tile">
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+              <div className="tile is-parent">
+                <img src="http://via.placeholder.com/400x150" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
