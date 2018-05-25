c173fb3ad04fad00adab8f1fc651f19f862c4a5e
meakommo <meako98@gmail.com>
2018-05-26 11:21:30 +1200

    added footer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import NavBar from './NavBar'
+import Footer from './Footer'
 
 class App extends React.Component {
   render () {
     return (
       <div>
         <NavBar />
+        <Footer />
       </div>
     )
   }
---
+++ client/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <div className="content has-text-centered">
+            <p>
+              These are some words Hello.
+            </p>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
