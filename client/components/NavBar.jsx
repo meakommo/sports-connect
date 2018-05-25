de4dc51e4e342657587af6ebf7ae99ca5dbc8ad7
Frank <xuzy213@gmail.com>
2018-05-26 10:40:04 +1200

    clean navbar component

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -4,18 +4,7 @@ import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
 class NavBar extends React.Component {
   render () {
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
-          </a>
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-      </nav>
+      <div>NavBar</div>
     )
   }
 }
