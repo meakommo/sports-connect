2a9299fe81b57b72874e4a8cb30763e815e2cac6
Frank <xuzy213@gmail.com>
2018-05-26 13:28:38 +1200

    clean up

--- client/apiClient.js
+++
@@ -1,9 +0,0 @@
-import {set} from './utils/localstorage'
-import consume from './utils/api'
-
-export function getAllPosts () {
-  return consume('get', '/api/v1/home/')
-    .then(res => {
-      return res.body
-    })
-}
--- client/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-
-import allPosts from './allPosts'
-
-const reducers = combineReducers({
-  allPosts
-})
-
-export default reducers
